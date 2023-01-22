69846537d639a46206e427dbdb614f617b52d3fd
fuzul <fuzuli@gmail.com>
2023-01-22 23:29:20 +1100

    Add project files.

---
+++ RankingApp/ClientApp/src/components/AlbumImages.js
@@ -0,0 +1,25 @@
+﻿import GodFather from '../images/movies/GodFather.png';
+import Highlander from '../images/movies/Highlander.png';
+import Highlander2 from '../images/movies/Highlander2.png';
+import LastOfTheMohicans from '../images/movies/LastOfTheMohicans.png';
+import PoliceAcademy6 from '../images/movies/PoliceAcademy6.png';
+import RearWindow from '../images/movies/RearWindow.png';
+import RoadHouse from '../images/movies/RoadHouse.png';
+import Shawshank from '../images/movies/Shawshank.png';
+import StarTreck4 from '../images/movies/StarTreck4.png';
+import Superman4 from '../images/movies/Superman4.png';
+
+const AlbumImagesArr = [
+    { id: 1, image: },
+    { id: 2, image: },
+    { id: 3, image: },
+    { id: 4, image: },
+    { id: 5, image: },
+    { id: 6, image: },
+    { id: 7, image: },
+    { id: 8, image: },
+    { id: 9, image: },
+    { id: 10, image: },
+];
+
+export default AlbumImagesArr;
---
+++ RankingApp/ClientApp/src/components/RankItems.js
@@ -0,0 +1,36 @@
+﻿import React, { useState, useEffect } from 'react';
+import MovieImageArr from "./MovieImages.js";
+import RankingGrid from "./RankingGrid";
+//import AlbumImageArr from './AlbumImages.js';
+
+const RankItems = () => {
+    const [items, setItems] = useState([]);
+    const dataType = 1;
+
+    useEffect(() => {
+        fetch(`item/${dataType}`)
+            .then((results) => {
+                return results.json();
+            })
+            .then(data => {
+                setItems(data);
+            })
+    }, [])
+
+    return (
+        <main>
+            <RankingGrid items={items} imgArr={MovieImageArr} />
+            <div className="items-not-ranked">
+                {
+                    (items.length > 0) ? items.map((item) =>
+                        //<h3>{item.title}</h3>
+                        <div className="unranked-cell">
+                            <img id={'item-${item.id}'} src={MovieImageArr.find(o => o.id === item.imageId)?.image} />
+                        </div>
+                ) : <div>Loading...</div>
+            }
+            </div>
+        </main>
+        )
+}
+export default RankItems;
